6254f39f8807859d4b34e57b5d8f19a4c90555c0
John Gustafson <John.Gustafson1@Marist.edu>
2022-01-13 22:13:44 -0500

    New Assets and styling for login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment as env } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    AuthModule.forRoot({
+      domain: env.Domain,
+      clientId: env.ClientID
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Domain: 'dev-79yt2oqa.us.auth0.com',
+  ClientID: 'IHHlLvafDMKmUc6qH6nRVkaFBfuNQQkO'
 };
 
 /*

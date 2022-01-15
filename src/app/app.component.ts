00f267c3d556ac8cc3f0567cc7158cb7cfdd55b5
John Gustafson <John.Gustafson1@Marist.edu>
2022-01-14 20:13:29 -0500

    Added loading screen on routing and when the auth is in a loading state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,57 @@
 import { Component } from '@angular/core';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+import { User } from './models/user';
 import { SanityService } from './shared/sanity.service';
-import { User } from './models/user'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ShareMe';
-  constructor(public authService: AuthService, private sanityService: SanityService) {
-    this.authService.user$.subscribe(user => {
+  title: string = 'ShareMe';
+  loading: boolean = false;
+
+  constructor(
+    public authService: AuthService,
+    private sanityService: SanityService,
+    private router: Router
+  ) {
+    this.authService.user$.subscribe((user) => {
       if (user) {
         const doc: User = {
           _id: user?.sub ? user.sub.replace('|', '-') : '-1',
           _type: 'user',
           userName: user?.name,
-          image: user?.picture
-        }
+          image: user?.picture,
+        };
         this.sanityService.addOrCreateUser(doc);
+      }
+    });
+
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.loading = false;
+          break;
+        }
+        default: {
+          break;
+        }
       }
     });
   }
